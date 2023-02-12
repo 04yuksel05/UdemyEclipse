03992e402e5c259c227818c0cd72d17a46206918
Yüksel Demirel <yuksel.demirel.89@gmail.com>
2023-02-13 02:56:32 +0300

    2.projem

--- src/main/java/com/yazaradi/proje/ikinci25.java
+++ src/main/java/com/yazaradi/proje/ikinci25.java
@@ -5,6 +5,7 @@ public class ikinci25 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
              System.out.println("Test Developer 123");
+             System.out.println("Test");
 	}
 
 }
