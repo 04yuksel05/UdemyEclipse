2fa141688febda46597b6c905cbec184f3098cdf
Yüksel Demirel <yuksel.demirel.89@gmail.com>
2023-02-13 02:23:41 +0300

    ilk projem

---
+++ src/main/java/com/yazaradi/proje/App.java
@@ -0,0 +1,13 @@
+package com.yazaradi.proje;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/yazaradi/proje/Deneme.java
@@ -0,0 +1,11 @@
+package com.yazaradi.proje;
+
+public class Deneme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git Github Eclipse");
+
+	}
+
+}
